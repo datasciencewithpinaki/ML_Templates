3ef10a00b46688b25193c49b4cc1eef35e087ce2
Pinaki Brahma <pinaki.brahma@gmail.com>
2021-08-21 17:15:02 +0530

    scaling and dim reduc added
    scaling and dim reduc added

--- helper_func_preprocess.py
+++ helper_func_preprocess.py
@@ -1,8 +1,14 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer, KNNImputer
 from sklearn.preprocessing import OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+
 
 class PreProcess:
     '''
@@ -13,6 +19,8 @@ class PreProcess:
     '''
     TEST_SIZE = 0.2
     RANDOM_STATE = 0
+    DIM_REDUC_METHOD = TSNE
+    DIM_REDUC_N_COMPONENTS = 2
 
     def __init__(self, df:pd.DataFrame, target_fetaure:str, numeric_features:list=None, catg_features:list=None, cols_w_manyNAs:list=None, cols_w_low_dev:list=None, multi_coll_featr:list=None):
         '''
@@ -31,33 +39,53 @@ class PreProcess:
         print(f"Target Feature: {self.target_feature}")
 
 
-    def preProcessData(self):
-        ## Create X & y from df
-        self.X = self.df[self.catg_features_upd + self.numeric_features_upd]
-        self.y = self.df[self.target_feature]
-        print(f"Shape of X: {self.X.shape}")
-        print(f"Shape of y: {self.y.shape}")
-        ## Split DF
-        self.splitDf()
-        ## Impute Outliers in Numeric Cols
-        self.X_train_imp = self.fitTransformOutl(self.X_train)
-        self.X_test_imp = self.transformOutl(self.X_test)
-        ## Missing Value Treatment - Numeric & Catg
-        self.X_train_na_imp = self.fitImputeNa(self.X_train, self.X_train_imp)
-        self.X_test_na_imp = self.transformImputeNa(self.X_test, self.X_test_imp)
-        print(self.X_train_na_imp.shape)
-        print(self.X_test_na_imp.shape)
-        ## Encoding Catg Features
-        self.getColsOheTargEncd()
-        self.X_train_na_imp = self.fit_transform_bktInfreqVal(self.X_train_na_imp)
-        self.X_test_na_imp = self.transform_bktInfreqVal(self.X_test_na_imp)
-        print(self.X_train_na_imp.shape)
-        print(self.X_test_na_imp.shape)
-        self.X_train_enc = self.combEncdNumCols(self.X_train_na_imp, training=True)
-        self.X_test_enc = self.combEncdNumCols(self.X_test_na_imp, training=False)
-        ## Scaling DF
+    def preProcessData(self, new_df=None, new_data=False, dim_reduc=False):
+        if new_data==False:
+            ## Create X & y from df
+            self.X = self.df[self.catg_features_upd + self.numeric_features_upd]
+            self.y = self.df[self.target_feature]
+            print(f"Shape of X: {self.X.shape}")
+            print(f"Shape of y: {self.y.shape}")
+            ## Split DF
+            self.splitDf()
+            ## Impute Outliers in Numeric Cols
+            self.X_train_imp = self.fitTransformOutl(self.X_train)
+            self.X_test_imp = self.transformOutl(self.X_test)
+            ## Missing Value Treatment - Numeric & Catg
+            self.X_train_na_imp = self.fitImputeNa(self.X_train, self.X_train_imp)
+            self.X_test_na_imp = self.transformImputeNa(self.X_test, self.X_test_imp)
+            ## Bucketing infrequent feature values into 'Other'
+            self.getColsOheTargEncd()
+            self.X_train_bkt = self.fit_transform_bktInfreqVal(self.X_train_na_imp, self.catg_features_upd)
+            self.X_test_bkt = self.transform_bktInfreqVal(self.X_test_na_imp, self.catg_features_upd)
+            ## Encoding Catg Features
+            self.X_train_enc = self.combEncdNumCols(self.X_train_bkt, self.catg_features_upd, training=True)
+            self.X_test_enc = self.combEncdNumCols(self.X_test_bkt, self.catg_features_upd, training=False)
+            ## Scaling DF
+            self.X_train_sc = self.scaleDf(self.X_train_enc, training=True, scaler=MinMaxScaler)
+            self.X_test_sc = self.scaleDf(self.X_test_enc, training=False)
+            ## Feature reduction like PCA or t-SNE
+            self.X_train_dim_reduc = self.processDimReduc(self.X_train_sc,
+                method=self.DIM_REDUC_METHOD, n_components=self.DIM_REDUC_N_COMPONENTS, training=True)
+            self.X_test_dim_reduc = self.processDimReduc(self.X_test_sc,
+                method=self.DIM_REDUC_METHOD, n_components=self.DIM_REDUC_N_COMPONENTS, training=False)
+            
 
-        ## Feature reduction like PCA or t-SNE
+        if new_data==True:
+            if new_df.shape[0]==0:
+                print(f"You haven't included the new data for processing")
+                return None
+            new_df_imp = self.transformOutl(new_df)
+            new_df_na_imp = self.transformImputeNa(new_df, new_df_imp)
+            new_df_bkt = self.transform_bktInfreqVal(new_df_na_imp, self.catg_features_upd)
+            new_df_enc = self.combEncdNumCols(new_df_bkt, self.catg_features_upd, training=False)
+            new_df_sc = self.scaleDf(new_df_enc, training=False)
+            if dim_reduc==True:
+                new_df_dim_reduc = self.processDimReduc(new_df_sc,
+                    self.DIM_REDUC_METHOD, self.DIM_REDUC_N_COMPONENTS, training=False)
+            else:
+                new_df_dim_reduc = new_df_sc
+            return new_df_dim_reduc
 
 
     ## ********* Split DF ********* ##
@@ -200,7 +228,7 @@ class PreProcess:
         self.cols_for_ohe = list(set(self.catg_features_upd).difference(set(self.cols_for_target_encd)))
 
 
-    def fit_transform_bktInfreqVal(self, df):
+    def fit_transform_bktInfreqVal(self, df, catg_col):
         '''
         Fitting on Training data
         check if some values in a column are very rare
@@ -208,14 +236,14 @@ class PreProcess:
         Helps in reducing OHE columns later
         '''
         temp_df = df.copy()
-        value_prop_dict = {col: temp_df[col].value_counts(normalize=True) for col in self.cols_for_ohe}
+        value_prop_dict = {col: temp_df[col].value_counts(normalize=True) for col in catg_col}
         self.value_prop_dict_final = {k: value_prop_dict[k][value_prop_dict[k] < 0.1].index.to_list() for k in value_prop_dict.keys()}
         for k, v in self.value_prop_dict_final.items():
             temp_df[k] = temp_df[k].map(lambda x: 'Other' if x in v else x)
         return temp_df
 
 
-    def transform_bktInfreqVal(self, df):
+    def transform_bktInfreqVal(self, df, catg_col):
         '''
         Transform Test data based on Fit
         check if some values in a column are very rare
@@ -228,31 +256,75 @@ class PreProcess:
         return temp_df
 
 
-    def getOheOutputCols(self, df):
-        col_ohe_val_dict = {col: list(df[col].unique()) for col in self.cols_for_ohe}
+    def getOheOutputCols(self, df, catg_col):
+        col_ohe_val_dict = {col: list(df[col].unique()) for col in catg_col}
         self.op_cols_for_ohe = [f"{k}_{v}" for k, vals in col_ohe_val_dict.items() for v in vals]
         print(f"len of op cols for OHE: {len(self.op_cols_for_ohe)}")
 
     
-    def combEncdNumCols(self, df, training:str='True'):
+    def combEncdNumCols(self, df, catg_col, training:str='True'):
         temp_df = df.copy()
         print(f"comb encd + num func: {temp_df.shape}")
         ## fit OHE on catg cols in training data
         if training:
-            self.getOheOutputCols(temp_df[self.cols_for_ohe])
+            self.getOheOutputCols(temp_df[catg_col], catg_col)
             self.encoder_catg = OneHotEncoder(handle_unknown='ignore', sparse=False)
-            temp_df_ohe = pd.DataFrame(self.encoder_catg.fit_transform(temp_df[self.cols_for_ohe]), columns=self.op_cols_for_ohe)
+            temp_df_ohe = pd.DataFrame(self.encoder_catg.fit_transform(temp_df[catg_col]), columns=self.op_cols_for_ohe)
         ## transform catg cols in test data
         else:  # training==False
-            temp_df_ohe = pd.DataFrame(self.encoder_catg.transform(temp_df[self.cols_for_ohe]), columns=self.op_cols_for_ohe)
+            temp_df_ohe = pd.DataFrame(self.encoder_catg.transform(temp_df[catg_col]), columns=self.op_cols_for_ohe)
         ## add back numeric columns to OHE cols
-        cols_left = list(set(temp_df.columns).difference(set(self.cols_for_ohe)))
+        cols_left = list(set(temp_df.columns).difference(set(catg_col)))
         temp_df_ohe2 = pd.concat([temp_df[cols_left], temp_df_ohe], axis=1)
         return temp_df_ohe2
 
 
     ## ********* Scaling DF ********* ##
+    def scaleDf(self, df, training=True, scaler=MinMaxScaler):
+        temp_df = df.copy()
+
+        if training==True:
+            if scaler==MinMaxScaler:
+                self.scaler_obj = MinMaxScaler(clip=True)
+            else:
+                self.scaler_obj = StandardScaler()
+            temp_df_sc = pd.DataFrame(self.scaler_obj.fit_transform(temp_df), 
+                            columns=df.columns.to_list())
+        else:  #training==False
+            temp_df_sc = pd.DataFrame(self.scaler_obj.transform(temp_df), 
+                            columns=df.columns.to_list())
+        return temp_df_sc
 
 
     ## ********* Feature reduction like PCA or t-SNE ********* ##
+    def processDimReduc(self, df, method=DIM_REDUC_METHOD, 
+                        n_components=DIM_REDUC_N_COMPONENTS, training=True):
+        temp_df = df.copy()
+        fact_cols = [f"FACT_{i+1}" for i in range(n_components)]
+        if training==True:
+            dim_reduc = method(n_components)
+            dim_reduc_result = dim_reduc.fit_transform(temp_df)
+            dim_reduc_result_df = pd.DataFrame(data = dim_reduc_result, columns = fact_cols)
+            self.dim_reduc = dim_reduc
+        if training==False:
+            dim_reduc_result = self.dim_reduc.transform(temp_df) if method==PCA else self.dim_reduc.fit_transform(temp_df)
+            dim_reduc_result_df = pd.DataFrame(data = dim_reduc_result, columns = fact_cols)
+        
+        return dim_reduc_result_df
+    
 
+    def dimReducPlot(self, df, y):
+        temp_df = df.copy()
+        temp_y = y.copy()
+        temp_df['label'] = temp_y.reset_index(drop=True)
+        fact_cols = temp_df.columns.to_list()
+        fig, ax = plt.subplots(1, figsize=(12, 6))
+        sns.scatterplot(x=fact_cols[0], y=fact_cols[1], hue=fact_cols[2], 
+            data=temp_df, ax=ax, s=120)
+        lim = {col: [temp_df[col].min(), temp_df[col].max()] for col in temp_df.columns}
+        ax.set_xlim(lim['FACT_1'])
+        ax.set_ylim(lim['FACT_2'])
+        ax.set_aspect('equal')
+        ax.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.0)
+        plt.title(f"Two dimensions explaing {self.target_feature} using {type(self.dim_reduc).__name__}")
+        plt.show()
